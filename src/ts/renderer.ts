877dc25d8c0b342d76fa164524a2bef9bd83311e
lewohy <lwh8762@gmail.com>
2020-02-11 02:06:49 +0900

    .

--- src/ts/renderer.ts
+++ src/ts/renderer.ts
@@ -228,7 +228,7 @@ export class Renderer {
      */
     private setupEvent(): void {
         this.element.addEventListener('wheel', e => {
-            this.zoomSize -= e.deltaY / Math.abs(e.deltaY) * 0.25;
+            this.zoomSize -= e.deltaY / Math.abs(e.deltaY);
 
             if (this.zoomSize <= 0) {
                 this.zoomSize = 1;
